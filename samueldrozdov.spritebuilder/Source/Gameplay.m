2ee325edb875fd1c468555c733435e42de36c4b0
sdrozzy <sdrozzy@gmail.com>
2014-07-08 13:56:22 -0400

    added accelerometer, but cannot test it yet

--- samueldrozdov.spritebuilder/Source/Gameplay.m
+++ samueldrozdov.spritebuilder/Source/Gameplay.m
@@ -7,10 +7,39 @@
 //
 
 #import "Gameplay.h"
+#import <CoreMotion/CoreMotion.h>
 
 @implementation Gameplay {
+    
+    // important: only create one instance of a motion manager
+    CMMotionManager *_motionManager;
+    CCLabelTTF *_label;
+    
     CCLabelTTF *_timeLabel;
     int time;
+}
+
+- (id)init
+{
+    if (self = [super init])
+    {
+        _label= [CCLabelTTF labelWithString:@"X" fontName:@"Arial" fontSize:48];
+        [self addChild:_label];
+        _motionManager = [[CMMotionManager alloc] init];
+    }
+    return self;
+}
+
+- (void)onEnter
+{
+    [super onEnter];
+    _label.position = ccp(self.contentSize.width/2, self.contentSize.height/2);
+    [_motionManager startAccelerometerUpdates];
+}
+- (void)onExit
+{
+    [super onExit];
+    [_motionManager stopAccelerometerUpdates];
 }
 
 // is called when CCB file has completed loading
@@ -26,7 +55,7 @@
     //
 }
 
-//tracks time
+// updates that happen in time
 - (void)update:(CCTime)delta {
     time -= .1;
     _timeLabel.string = [NSString stringWithFormat:@"%d", time];
@@ -35,6 +64,13 @@
         CCScene *recapScene = [CCBReader loadAsScene:@"Recap"];
         [[CCDirector sharedDirector] replaceScene:recapScene];
     }
+    
+    // accelerometer data to be updated
+    CMAccelerometerData *accelerometerData = _motionManager.accelerometerData;
+    CMAcceleration acceleration = accelerometerData.acceleration;
+    CGFloat newXPosition = _label.position.x + acceleration.y * 1000 * delta;
+    newXPosition = clampf(newXPosition, 0, self.contentSize.width);
+    _label.position = CGPointMake(newXPosition, _label.position.y);
 }
 
 @end
