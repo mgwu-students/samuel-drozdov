79755483d182d5ebc33accc247bfe9fe0e0cdaa9
sdrozzy <sdrozzy@gmail.com>
2014-07-09 12:37:44 -0400

    accelerometer implemented but not moving the crosshair

--- samueldrozdov.spritebuilder/Source/Gameplay.m
+++ samueldrozdov.spritebuilder/Source/Gameplay.m
@@ -8,27 +8,29 @@
 
 #import "Gameplay.h"
 #import "Ball.h"
+#import "Crosshair.h"
 #import <CoreMotion/CoreMotion.h>
 
 @implementation Gameplay {
     
-    // important: only create one instance of a motion manager
+    // motion manager (cannot make more than one)
     CMMotionManager *_motionManager;
-    CCLabelTTF *_label;
     
+    // time variables
     CCLabelTTF *_timeLabel;
     int time;
     
+    // moving nodes
     Ball *ball;
-    
+    Crosshair *crosshair;
 }
 
 - (id)init
 {
     if (self = [super init])
     {
-        _label= [CCLabelTTF labelWithString:@"X" fontName:@"Arial" fontSize:48];
-        [self addChild:_label];
+        crosshair = (Crosshair*)[CCBReader load:@"Crosshair"];
+        [self addChild:crosshair z:5]; // high 'z' value so its visible and over the other nodes
         _motionManager = [[CMMotionManager alloc] init];
     }
     return self;
@@ -37,7 +39,7 @@
 - (void)onEnter
 {
     [super onEnter];
-    _label.position = ccp(self.contentSize.width/2, self.contentSize.height/2);
+    crosshair.position = ccp(self.contentSize.width/2, self.contentSize.height/2);
     [_motionManager startAccelerometerUpdates];
 }
 - (void)onExit
@@ -56,12 +58,14 @@
     // find the size of the gameplay scene
     CGSize bbsize = [[UIScreen mainScreen] bounds].size;
     // position the ball randomly in the gameplay scene
-    ball.position = ccp(arc4random_uniform(bbsize.width-70),arc4random_uniform(bbsize.height-70));
+    ball.position = ccp(arc4random_uniform(bbsize.width - 70),arc4random_uniform(bbsize.height - 70));
     // add the ball to the Gameplay scene
     [self addChild:ball];
     
-    time = 900;
+    time = 600;
 }
+
+
 
 
 // called on every touch in this scene
@@ -71,20 +75,26 @@
 
 // updates that happen in time
 - (void)update:(CCTime)delta {
+    
+    // updates the time counter
     time -= .1;
     _timeLabel.string = [NSString stringWithFormat:@"%d", time];
+    
+    
+    // accelerometer data to be updated
+    CMAccelerometerData *accelerometerData = _motionManager.accelerometerData;
+    CMAcceleration acceleration = accelerometerData.acceleration;
+    CGFloat newXPosition = crosshair.position.x + acceleration.y * 1000 * delta;
+    
+    newXPosition = clampf(newXPosition, 0, self.contentSize.width);
+    crosshair.position = CGPointMake(newXPosition, crosshair.position.y);
+    
     
     if(time == 0) {
         CCScene *recapScene = [CCBReader loadAsScene:@"Recap"];
         [[CCDirector sharedDirector] replaceScene:recapScene];
     }
     
-    // accelerometer data to be updated
-    CMAccelerometerData *accelerometerData = _motionManager.accelerometerData;
-    CMAcceleration acceleration = accelerometerData.acceleration;
-    CGFloat newXPosition = _label.position.x + acceleration.y * 1000 * delta;
-    newXPosition = clampf(newXPosition, 0, self.contentSize.width);
-    _label.position = CGPointMake(newXPosition, _label.position.y);
 }
 
 @end
